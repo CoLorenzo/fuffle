c2c42ce3c990d5a816e2fec56ccf1ebb2e5be05e
Lorenzo Berardi <lorenzo2berardi@gmail.com>
2026-08-28 11:47:01 +0200

    chore: bump version to 1.4.0

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ const (
 	alphanumericChars    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 )
 
-const version = "1.3.0"
+const version = "1.4.0"
 
 func main() {
 	if len(os.Args) < 2 {
